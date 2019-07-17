e2ce9065b276bfa3639f27d520ee75193ee02a39
Pieter van Halem <Pieter.vanHalem@vanoord.com>
2019-07-17 15:35:07 +0200

    update __init__

--- docs/conf.py
+++ docs/conf.py
@@ -82,7 +82,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'HALEM'
-copyright = u'2019, Pietje'
+copyright = u'2019, Pieter van Halem'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
