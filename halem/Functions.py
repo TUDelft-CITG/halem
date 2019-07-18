0458049423160646b43221e00cd0a40b19bd01a4
Pieter van Halem <Pieter.vanHalem@vanoord.com>
2019-07-18 13:24:52 +0200

    minor fix

--- halem/Functions.py
+++ halem/Functions.py
@@ -95,9 +95,9 @@ def costfunction_timeseries(edge,V_max, WD_min ,flow, WVPI, L, tria):
     u_w = flow.u[IB[0]]
     WD_W = flow.WD[IB[0]]
     for i in range(1, len(IB)):
-        v_w += flow.v[IB[i]]
-        u_w += flow.u[IB[i]]
-        WD_W += flow.WD[IB[i]]
+        v_w =v_w +  flow.v[IB[i]]
+        u_w = u_w +  flow.u[IB[i]]
+        WD_W =WD_W +  flow.WD[IB[i]]
        
     v_w = (v_w / len(IB))
     u_w = (u_w / len(IB))
