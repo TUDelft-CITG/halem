e61ae1fd9fdfc4a4971f54cc7dc4cf9841152345
Pieter van Halem <Pieter.vanHalem@vanoord.com>
2019-08-05 15:01:35 +0200

    no message

--- halem/Base_functions.py
+++ halem/Base_functions.py
@@ -16,10 +16,8 @@ import datetime, time
 from datetime import datetime
 
 def save_object(obj, filename):
-        """" Method for saving the Roadmap
-        """"
-        with open(filename, 'wb') as output:  # Overwrites any existing file.
-                pickle.dump(obj, output, pickle.HIGHEST_PROTOCOL)
+    with open(filename, 'wb') as output:  # Overwrites any existing file.
+       pickle.dump(obj, output, pickle.HIGHEST_PROTOCOL)
 
 def plot_timeseries(route, Roadmap):
     dist = []
