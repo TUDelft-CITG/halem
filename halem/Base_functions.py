da2321cf6cfe524fc6c9b06bab8d98dc0016b220
Pieter van Halem <Pieter.vanHalem@vanoord.com>
2019-08-13 11:39:08 +0200

    clean-up start

--- halem/Base_functions.py
+++ halem/Base_functions.py
@@ -3,15 +3,9 @@ import halem.Functions as Functions
 import halem.Calc_path as Calc_path
 import halem.Flow_class as Flow_class
 
-import math
 import numpy as np
-from numpy import ma
 import pickle
-
-import os
 import matplotlib.pyplot as plt
-from matplotlib import pyplot as plt
-from matplotlib import animation
 import datetime, time
 from datetime import datetime
 
@@ -19,31 +13,6 @@ from datetime import datetime
 def save_object(obj, filename):
     with open(filename, "wb") as output:  # Overwrites any existing file.
         pickle.dump(obj, output, pickle.HIGHEST_PROTOCOL)
-
-
-def plot_timeseries(route, Roadmap):
-    dist = []
-    TT = []
-    D = 0
-    for i in range(route.route[:, 0].shape[0] - 1):
-        D = D + Mesh_maker.haversine(
-            (route.y_route[i], route.x_route[i]),
-            (route.y_route[i + 1], route.x_route[i + 1]),
-        )
-        dist.append(D)
-        T = Roadmap.mask[int(route.route[i, 0])]
-        TT.append(T)
-    TT = np.array(TT)
-    dist = np.array(dist)
-
-    plt.plot(dist, route.route[:-1, 1], "ro")
-    cval = np.arange(0, 1.1, 0.5)
-    plt.contourf(dist, Roadmap.t, np.transpose(TT), cval)
-    plt.colorbar()
-    plt.xlabel("traveled distance [m]")
-    plt.ylabel("time [s]")
-
-    plt.ylim(route.route[0, 1] - 2000, route.route[-1, 1] + 2000)
 
 
 def plot_timeseries2(path, time, Roadmap, Color="r"):
@@ -105,129 +74,49 @@ def plot_timeseries2(path, time, Roadmap, Color="r"):
         plt.ylabel("time [h]")
         plt.legend(loc="best")
 
+def HALEM_func(start, stop, t0, vmax, Roadmap, costfunction):
+    start = start[::-1]
+    stop = stop[::-1]
+
+    vvmax = Roadmap.vship[:, -1]
+    vv = np.abs(vvmax - vmax)
+    arg_vship = int(np.argwhere(vv == vv.min())[0])
+
+    class graph_functions_time:
+        weights = type[arg_vship].weights
+        time = Roadmap.weight_time[arg_vship].weights
+        vship = Roadmap.vship[arg_vship]
+
+    route = Calc_path.Has_route(start, stop, Roadmap, t0, graph_functions_time)
+    path = Roadmap.nodes[np.array(route.route[:, 0], dtype=int)]
+    time = route.route[:, 1]
+
+    dist = []
+    D = 0
+    for i in range(route.route[:, 0].shape[0] - 1):
+        D = D + Mesh_maker.haversine(
+            (route.y_route[i], route.x_route[i]),
+            (route.y_route[i + 1], route.x_route[i + 1]),
+        )
+        dist.append(D)
+    dist = np.array(dist)
+    return path[:, ::-1], time, dist
 
 def HALEM_time(start, stop, t0, vmax, Roadmap):
-    start = start[::-1]
-    stop = stop[::-1]
+    costfunction = Roadmap.weight_time
+    return HALEM_func(start, stop, t0, vmax, Roadmap, costfunction)
 
-    vvmax = Roadmap.vship[:, -1]
-    vv = np.abs(vvmax - vmax)
-    arg_vship = int(np.argwhere(vv == vv.min())[0])
-    # arg_vship = arg_vship if arg_vship == 0 or vmax < vvmax[-1]  else arg_vship - 1
-
-    class graph_functions_time:
-        function_type = "time optimalisation"
-        weights = Roadmap.weight_time[arg_vship].weights
-        time = Roadmap.weight_time[arg_vship].weights
-        vship = Roadmap.vship[arg_vship]
-
-    route = Calc_path.Has_route(start, stop, Roadmap, t0, graph_functions_time)
-    path = Roadmap.nodes[np.array(route.route[:, 0], dtype=int)]
-    time = route.route[:, 1]
-
-    dist = []
-    D = 0
-    for i in range(route.route[:, 0].shape[0] - 1):
-        D = D + Mesh_maker.haversine(
-            (route.y_route[i], route.x_route[i]),
-            (route.y_route[i + 1], route.x_route[i + 1]),
-        )
-        dist.append(D)
-    dist = np.array(dist)
-    return path[:, ::-1], time, dist
-
-
+    
 def HALEM_space(start, stop, t0, vmax, Roadmap):
-    start = start[::-1]
-    stop = stop[::-1]
-
-    vvmax = Roadmap.vship[:, -1]
-
-    vv = np.abs(vvmax - vmax)
-    arg_vship = int(np.argwhere(vv == vv.min())[0])
-    # arg_vship = arg_vship if arg_vship == 0 or vmax < vvmax[-1]  else arg_vship - 1
-
-    class graph_functions_time:
-        function_type = "time optimalisation"
-        weights = Roadmap.weight_space[arg_vship].weights
-        time = Roadmap.weight_time[arg_vship].weights
-        vship = Roadmap.vship[arg_vship]
-
-    route = Calc_path.Has_route(start, stop, Roadmap, t0, graph_functions_time)
-    path = Roadmap.nodes[np.array(route.route[:, 0], dtype=int)]
-    time = route.route[:, 1]
-
-    dist = []
-    D = 0
-    for i in range(route.route[:, 0].shape[0] - 1):
-        D = D + Mesh_maker.haversine(
-            (route.y_route[i], route.x_route[i]),
-            (route.y_route[i + 1], route.x_route[i + 1]),
-        )
-        dist.append(D)
-    dist = np.array(dist)
-    return path[:, ::-1], time, dist
+    costfunction = Roadmap.weight_space
+    return HALEM_func(start, stop, t0, vmax, Roadmap, costfunction)
 
 
 def HALEM_cost(start, stop, t0, vmax, Roadmap):
-    start = start[::-1]
-    stop = stop[::-1]
-
-    vvmax = Roadmap.vship[:, -1]
-
-    vv = np.abs(vvmax - vmax)
-    arg_vship = int(np.argwhere(vv == vv.min())[0])
-    # arg_vship = arg_vship if arg_vship == 0 or vmax < vvmax[-1]  else arg_vship - 1
-
-    class graph_functions_time:
-        function_type = "time optimalisation"
-        weights = Roadmap.weight_cost[arg_vship].weights
-        time = Roadmap.weight_time[arg_vship].weights
-        vship = Roadmap.vship[arg_vship]
-
-    route = Calc_path.Has_route(start, stop, Roadmap, t0, graph_functions_time)
-    path = Roadmap.nodes[np.array(route.route[:, 0], dtype=int)]
-    time = route.route[:, 1]
-
-    dist = []
-    D = 0
-    for i in range(route.route[:, 0].shape[0] - 1):
-        D = D + Mesh_maker.haversine(
-            (route.y_route[i], route.x_route[i]),
-            (route.y_route[i + 1], route.x_route[i + 1]),
-        )
-        dist.append(D)
-    dist = np.array(dist)
-    return path[:, ::-1], time, dist
+    costfunction = Roadmap.weight_cost
+    return HALEM_func(start, stop, t0, vmax, Roadmap, costfunction)
 
 
 def HALEM_co2(start, stop, t0, vmax, Roadmap):
-    start = start[::-1]
-    stop = stop[::-1]
-
-    vvmax = Roadmap.vship[:, -1]
-
-    vv = np.abs(vvmax - vmax)
-    arg_vship = int(np.argwhere(vv == vv.min())[0])
-    # arg_vship = arg_vship if arg_vship == 0 or vmax < vvmax[-1]  else arg_vship - 1
-
-    class graph_functions_time:
-        function_type = "time optimalisation"
-        weights = Roadmap.weight_co2[arg_vship].weights
-        time = Roadmap.weight_time[arg_vship].weights
-        vship = Roadmap.vship[arg_vship]
-
-    route = Calc_path.Has_route(start, stop, Roadmap, t0, graph_functions_time)
-    path = Roadmap.nodes[np.array(route.route[:, 0], dtype=int)]
-    time = route.route[:, 1]
-
-    dist = []
-    D = 0
-    for i in range(route.route[:, 0].shape[0] - 1):
-        D = D + Mesh_maker.haversine(
-            (route.y_route[i], route.x_route[i]),
-            (route.y_route[i + 1], route.x_route[i + 1]),
-        )
-        dist.append(D)
-    dist = np.array(dist)
-    return path[:, ::-1], time, dist
+    costfunction = Roadmap.weight_co2
+    return HALEM_func(start, stop, t0, vmax, Roadmap, costfunction)
--- halem/__init__.py
+++ halem/__init__.py
@@ -9,7 +9,6 @@ __version__ = "0.3.0"
 from pkg_resources import get_distribution, DistributionNotFound
 
 try:
-    # Change here if project is renamed and does not equal the package name
     dist_name = "halem"
     __version__ = get_distribution(dist_name).version
 except DistributionNotFound:
