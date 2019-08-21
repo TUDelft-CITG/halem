ef1791a4aa9b69fd82911d85b5893f64a80ba3cf
Pieter van Halem <Pieter.vanHalem@vanoord.com>
2019-08-21 14:17:18 +0200

    bugfix - runtime errors

--- halem/Functions.py
+++ halem/Functions.py
@@ -68,9 +68,11 @@ def Squat(h, T, V_max, LWL, WWL, ukc, WVPI):
         - 0.1159 * (np.sqrt(WWL * T) / RH) ** 2
         + 0.0191 * (np.sqrt(WWL * T) / RH) ** 3
     )
-
     V1inf2 = (np.exp(ghv2) - np.exp(-ghv2)) / (np.exp(ghv2) + np.exp(-ghv2))
-    V1Vinf = (V1inf2) ** 0.5 if V1inf2 > 0 else np.inf
+
+    V1Vinf = 0*V1inf2
+    for i in range(len(V1inf2)):
+        V1Vinf[i] = (V1inf2[i]) ** 0.5 if V1inf2[i] > 0 else np.inf
 
     V_grens = V_max * V1Vinf * VhV1
     V_squat_max = np.zeros(len(h))
