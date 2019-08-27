f37a08f18c9efd4259c158621fcccb8277bb3e19
Pieter van Halem <Pieter.vanHalem@vanoord.com>
2019-08-27 15:58:05 +0200

    no message

--- halem/Functions.py
+++ halem/Functions.py
@@ -155,7 +155,9 @@ def costfunction_timeseries(edge, V_max, WD_min, flow, WVPI, L, tria):
     yfrom = flow.nodes[edge[0]][0]
     xto = flow.nodes[edge[1]][1]
     yto = flow.nodes[edge[1]][0]
+    
     IB = inbetweenpoints(edge[0], edge[1], L, tria)
+
     v_w = flow.v[IB[0]]
     u_w = flow.u[IB[0]]
     WD_W = flow.WD[IB[0]]
@@ -171,7 +173,8 @@ def costfunction_timeseries(edge, V_max, WD_min, flow, WVPI, L, tria):
     u_w = u_w / len(IB)
     U_w = (u_w ** 2 + v_w ** 2) ** 0.5
 
-    vship = Squat(WD_W, WD_min, V_max, flow.LWL, flow.WWL, flow.ukc, WVPI)
+    # vship = Squat(WD_W, WD_min, V_max, flow.LWL, flow.WWL, flow.ukc, WVPI)
+    vship = V_max + 0 * WD_W
 
     alpha1 = np.arctan2((yto - yfrom), (xto - xfrom))
     alpha2 = np.arctan2(v_w, u_w) - alpha1
